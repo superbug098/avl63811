af46fd8b7b82bd1772979f348bd19d9272111d9a
superbug098 <superbug098@gmail.com>
2026-08-10 18:58:02 +0800

    Add files via upload

---
+++ linuxdvb/.dvb-usb-anysee.ko.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/media/usb/dvb-usb-v2/dvb-usb-anysee.ko := ld.lld -r -EL  -maarch64elf -z noexecstack --build-id=sha1  -T scripts/module.lds -o drivers/media/usb/dvb-usb-v2/dvb-usb-anysee.ko drivers/media/usb/dvb-usb-v2/dvb-usb-anysee.o drivers/media/usb/dvb-usb-v2/dvb-usb-anysee.mod.o;  make -f ./arch/arm64/Makefile.postlink drivers/media/usb/dvb-usb-v2/dvb-usb-anysee.ko
---
+++ linuxdvb/.dvb-usb-lmedm04.ko.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/media/usb/dvb-usb-v2/dvb-usb-lmedm04.ko := ld.lld -r -EL  -maarch64elf -z noexecstack --build-id=sha1  -T scripts/module.lds -o drivers/media/usb/dvb-usb-v2/dvb-usb-lmedm04.ko drivers/media/usb/dvb-usb-v2/dvb-usb-lmedm04.o drivers/media/usb/dvb-usb-v2/dvb-usb-lmedm04.mod.o;  make -f ./arch/arm64/Makefile.postlink drivers/media/usb/dvb-usb-v2/dvb-usb-lmedm04.ko
